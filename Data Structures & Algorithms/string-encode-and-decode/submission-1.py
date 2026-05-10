9d5efa6c96614838530cacbeb0b0ec47369045f0
Mohi <67804307+mohisyed@users.noreply.github.com>
2026-05-10 15:04:31 -0600

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    # delimiter: ¡
+    # string
+
+    # key will be string and the value will be len of string
+    
+    totalMap = []
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for x in strs:
+            res += str(len(x)) + "#" + x
+
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        res, i = [], 0
+        
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j + 1: j + 1 + length])
+            i = j + 1 + length
+        return res
